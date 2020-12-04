ac656657e73ca8c4a5dc1920c98abe2ce90a79bf
Gaurav Narula <gnarula94@gmail.com>
2020-12-04 18:02:08 +0100

    Add cli and minogrpc controller

---
+++ cli/node/builder.go
@@ -0,0 +1,372 @@
+// This file contains the implementation of a CLI builder.
+//
+// Documentation Last Review: 13.10.20202
+//
+
+package node
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+
+	ucli "github.com/urfave/cli/v2"
+	"go.dedis.ch/dela"
+	"go.dedis.ch/dela/cli"
+	"golang.org/x/xerrors"
+)
+
+// CLIBuilder is an application builder that will build a CLI to start and
+// control a node.
+//
+// - implements node.Builder
+type CLIBuilder struct {
+	daemonFactory DaemonFactory
+	injector      Injector
+	actions       *actionMap
+	startFlags    []cli.Flag
+	commands      []*cliCommand
+	inits         []Initializer
+	writer        io.Writer
+
+	// In production, the daemon is stopped via SIGTERM. In case of testing, the
+	// channel will be closed instead, because of instability.
+	enableSignal bool
+	sigs         chan os.Signal
+}
+
+// NewBuilder returns a new empty builder.
+func NewBuilder(inits ...Initializer) *CLIBuilder {
+	return NewBuilderWithCfg(nil, nil, inits...)
+}
+
+// NewBuilderWithCfg returns a new empty builder with specific configurations.
+func NewBuilderWithCfg(sigs chan os.Signal, out io.Writer, inits ...Initializer) *CLIBuilder {
+	if out == nil {
+		out = os.Stdout
+	}
+
+	enabled := false
+
+	if sigs == nil {
+		sigs = make(chan os.Signal, 1)
+		enabled = true
+	}
+
+	injector := NewInjector()
+
+	actions := &actionMap{}
+
+	factory := socketFactory{
+		injector: injector,
+		actions:  actions,
+		out:      out,
+	}
+
+	return &CLIBuilder{
+		injector:      injector,
+		actions:       actions,
+		daemonFactory: factory,
+		enableSignal:  enabled,
+		sigs:          sigs,
+		inits:         inits,
+		writer:        out,
+	}
+}
+
+// SetCommand implements node.Builder. It creates a new command and return its
+// builder.
+func (b *CLIBuilder) SetCommand(name string) cli.CommandBuilder {
+	cb := &cliCommand{name: name}
+	b.commands = append(b.commands, cb)
+	return cb
+}
+
+// SetStartFlags implements node.Builder. It appends the given flags to the list
+// of flags that will be used to create the start command.
+func (b *CLIBuilder) SetStartFlags(flags ...cli.Flag) {
+	b.startFlags = append(b.startFlags, flags...)
+}
+
+// MakeAction implements node.Builder. It creates a CLI action from the
+// template.
+func (b *CLIBuilder) MakeAction(tmpl ActionTemplate) cli.Action {
+	index := b.actions.Set(tmpl)
+
+	return func(c cli.Flags) error {
+		client, err := b.daemonFactory.ClientFromContext(c)
+		if err != nil {
+			return xerrors.Errorf("couldn't make client: %v", err)
+		}
+
+		// Encode the action ID over 2 bytes.
+		id := make([]byte, 2)
+		binary.LittleEndian.PutUint16(id, index)
+
+		// Prepare a set of flags that will be transmitted to the daemon so that
+		// the action has access to the same flags and their values.
+		fset := make(FlagSet)
+		lookupFlags(fset, c.(*ucli.Context))
+
+		buf, err := json.Marshal(fset)
+		if err != nil {
+			return xerrors.Errorf("failed to marshal flag set: %v", err)
+		}
+
+		err = client.Send(append(id, buf...))
+		if err != nil {
+			return xerrors.Opaque(err)
+		}
+
+		return nil
+	}
+}
+
+func lookupFlags(fset FlagSet, ctx *ucli.Context) {
+	for _, ancestor := range ctx.Lineage() {
+		if ancestor.Command != nil {
+			fill(fset, ancestor.Command.Flags, ancestor)
+		}
+
+		if ancestor.App != nil {
+			fill(fset, ancestor.App.Flags, ancestor)
+		}
+	}
+}
+
+func fill(fset FlagSet, flags []ucli.Flag, ctx *ucli.Context) {
+	for _, flag := range flags {
+		names := flag.Names()
+		if len(names) > 0 {
+			fset[names[0]] = convert(ctx.Value(names[0]))
+		}
+	}
+}
+
+func convert(v interface{}) interface{} {
+	switch value := v.(type) {
+	case ucli.StringSlice:
+		// StringSlice is an edge-case as it won't serialize correctly with JSON
+		// so we ask for the actual []string to allow a correct serialization.
+		return value.Value()
+	default:
+		return v
+	}
+}
+
+// Build implements node.Builder. It returns the application.
+func (b *CLIBuilder) Build() cli.Application {
+	for _, controller := range b.inits {
+		controller.SetCommands(b)
+	}
+
+	commands := b.buildCommands(b.commands)
+
+	commands = append(commands, &ucli.Command{
+		Name:   "start",
+		Usage:  "start the daemon",
+		Flags:  b.buildFlags(b.startFlags),
+		Action: b.start,
+	})
+
+	app := &ucli.App{
+		Name:  "Dela",
+		Usage: "Dedis Ledger Architecture",
+		Flags: []ucli.Flag{
+			&ucli.PathFlag{
+				Name:  "config",
+				Usage: "path to the config folder",
+				Value: ".dela",
+			},
+		},
+		Commands: commands,
+		Writer:   b.writer,
+	}
+
+	return app
+}
+
+func (b *CLIBuilder) start(c *ucli.Context) error {
+	if b.enableSignal {
+		signal.Notify(b.sigs, syscall.SIGINT, syscall.SIGTERM)
+
+		defer signal.Stop(b.sigs)
+	}
+
+	dir := c.Path("config")
+	if dir != "" {
+		err := os.MkdirAll(dir, 0700)
+		if err != nil {
+			return xerrors.Errorf("couldn't make path: %v", err)
+		}
+	}
+
+	daemon, err := b.daemonFactory.DaemonFromContext(c)
+	if err != nil {
+		return xerrors.Errorf("couldn't make daemon: %v", err)
+	}
+
+	for _, controller := range b.inits {
+		err = controller.OnStart(c, b.injector)
+		if err != nil {
+			return xerrors.Errorf("couldn't run the controller: %v", err)
+		}
+	}
+
+	// Daemon is started after the controllers so that everything has started
+	// when the daemon is available.
+	err = daemon.Listen()
+	if err != nil {
+		return xerrors.Errorf("couldn't start the daemon: %v", err)
+	}
+
+	defer daemon.Close()
+
+	<-b.sigs
+	signal.Stop(b.sigs)
+
+	// Controllers are stopped in reverse order so that high level components
+	// are stopped before lower level ones (i.e. stop a service before the
+	// database to avoid errors).
+	for i := len(b.inits) - 1; i >= 0; i-- {
+		err = b.inits[i].OnStop(b.injector)
+		if err != nil {
+			return xerrors.Errorf("couldn't stop controller: %v", err)
+		}
+	}
+
+	dela.Logger.Trace().Msg("daemon has been stopped")
+
+	return nil
+}
+
+func (b *CLIBuilder) buildCommands(in []*cliCommand) []*ucli.Command {
+	if len(in) == 0 {
+		return nil
+	}
+
+	commands := make([]*ucli.Command, len(in))
+	for i, command := range in {
+		cmd := &ucli.Command{
+			Name:  command.name,
+			Usage: command.description,
+			Flags: b.buildFlags(command.flags),
+		}
+
+		if command.action != nil {
+			cmd.Action = b.buildAction(command.action)
+		}
+
+		cmd.Subcommands = b.buildCommands(command.subcommands)
+
+		commands[i] = cmd
+	}
+
+	return commands
+}
+
+func (b *CLIBuilder) buildAction(a cli.Action) func(*ucli.Context) error {
+	return func(ctx *ucli.Context) error {
+		return a(ctx)
+	}
+}
+
+func (b *CLIBuilder) buildFlags(in []cli.Flag) []ucli.Flag {
+	flags := make([]ucli.Flag, len(in))
+	for i, input := range in {
+		switch flag := input.(type) {
+		case cli.StringFlag:
+			flags[i] = &ucli.StringFlag{
+				Name:     flag.Name,
+				Usage:    flag.Usage,
+				Required: flag.Required,
+				Value:    flag.Value,
+			}
+		case cli.StringSliceFlag:
+			flags[i] = &ucli.StringSliceFlag{
+				Name:     flag.Name,
+				Usage:    flag.Usage,
+				Required: flag.Required,
+				Value:    ucli.NewStringSlice(flag.Value...),
+			}
+		case cli.DurationFlag:
+			flags[i] = &ucli.DurationFlag{
+				Name:     flag.Name,
+				Usage:    flag.Usage,
+				Required: flag.Required,
+				Value:    flag.Value,
+			}
+		case cli.IntFlag:
+			flags[i] = &ucli.IntFlag{
+				Name:     flag.Name,
+				Usage:    flag.Usage,
+				Required: flag.Required,
+				Value:    flag.Value,
+			}
+		default:
+			panic(fmt.Sprintf("flag type '%T' not supported", input))
+		}
+	}
+
+	return flags
+}
+
+// CLICommand is a command builder to set the properties of a command.
+//
+// - implements cli.CommandBuilder
+type cliCommand struct {
+	name        string
+	description string
+	action      cli.Action
+	flags       []cli.Flag
+	subcommands []*cliCommand
+}
+
+// SetDescription implements cli.CommandBuilder. It sets the description of the
+// command.
+func (c *cliCommand) SetDescription(value string) {
+	c.description = value
+}
+
+// SetAction implements cli.CommandBuilder. It sets the action to invoked for
+// the command.
+func (c *cliCommand) SetAction(a cli.Action) {
+	c.action = a
+}
+
+// SetFlags implements cli.CommandBuilder. It defines the list of flags that can
+// be passed by the client.
+func (c *cliCommand) SetFlags(flags ...cli.Flag) {
+	c.flags = flags
+}
+
+// SetSubCommand implements cli.CommandBuilder. It creates a new subcommand and
+// returns its builder.
+func (c *cliCommand) SetSubCommand(name string) cli.CommandBuilder {
+	sub := &cliCommand{name: name}
+	c.subcommands = append(c.subcommands, sub)
+
+	return sub
+}
+
+// ActionMap stores actions and assigns a unique index to each.
+type actionMap struct {
+	list []ActionTemplate
+}
+
+func (m *actionMap) Set(a ActionTemplate) uint16 {
+	m.list = append(m.list, a)
+	return uint16(len(m.list) - 1)
+}
+
+func (m *actionMap) Get(index uint16) ActionTemplate {
+	if int(index) >= len(m.list) {
+		return nil
+	}
+
+	return m.list[index]
+}
---
+++ cli/node/builder_test.go
@@ -0,0 +1,171 @@
+package node
+
+import (
+	"flag"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	ucli "github.com/urfave/cli/v2"
+	"go.dedis.ch/dela/cli"
+	"go.dedis.ch/dela/internal/testing/fake"
+)
+
+func TestCliBuilder_Command(t *testing.T) {
+	builder := &CLIBuilder{}
+
+	cmd := builder.SetCommand("test")
+	require.NotNil(t, cmd)
+	require.Len(t, builder.commands, 1)
+}
+
+func TestCliBuilder_SetStartFlags(t *testing.T) {
+	builder := &CLIBuilder{}
+
+	builder.SetStartFlags(cli.StringFlag{}, cli.IntFlag{})
+	require.Len(t, builder.startFlags, 2)
+}
+
+func TestCliBuilder_Start(t *testing.T) {
+	builder := NewBuilder(fakeInitializer{})
+
+	builder.sigs <- syscall.SIGTERM
+	close(builder.sigs)
+
+	err := builder.start(nil)
+	require.NoError(t, err)
+}
+
+func TestCliBuilder_ForbiddenFolder_Start(t *testing.T) {
+	builder := NewBuilder(fakeInitializer{})
+
+	fset := flag.NewFlagSet("", 0)
+	fset.String("config", "\x00", "")
+
+	ctx := ucli.NewContext(nil, fset, nil)
+
+	err := builder.start(ctx)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "couldn't make path: mkdir \x00: ")
+
+}
+
+func TestCliBuilder_FailedDaemon_Start(t *testing.T) {
+	builder := NewBuilder(fakeInitializer{})
+
+	builder.daemonFactory = fakeFactory{err: fake.GetError()}
+
+	err := builder.start(nil)
+	require.EqualError(t, err, fake.Err("couldn't make daemon"))
+}
+
+func TestCliBuilder_FailStartDaemon_Start(t *testing.T) {
+	builder := NewBuilder(fakeInitializer{})
+
+	builder.daemonFactory = fakeFactory{errDaemon: fake.GetError()}
+
+	err := builder.start(nil)
+	require.EqualError(t, err, fake.Err("couldn't start the daemon"))
+}
+
+func TestCliBuilder_FailStartComponent_Start(t *testing.T) {
+	builder := NewBuilder(fakeInitializer{err: fake.GetError()})
+
+	err := builder.start(nil)
+	require.EqualError(t, err, fake.Err("couldn't run the controller"))
+}
+
+func TestCliBuilder_FailStopComponent_Start(t *testing.T) {
+	builder := NewBuilder(fakeInitializer{errStop: fake.GetError()})
+	builder.enableSignal = false
+	close(builder.sigs)
+
+	err := builder.start(nil)
+	require.EqualError(t, err, fake.Err("couldn't stop controller"))
+}
+
+func TestCliBuilder_MakeAction(t *testing.T) {
+	calls := &fake.Call{}
+	builder := &CLIBuilder{
+		actions:       &actionMap{},
+		daemonFactory: fakeFactory{calls: calls},
+	}
+
+	fset := flag.NewFlagSet("", 0)
+	fset.Var(ucli.NewStringSlice("item 1", "item 2"), "flag-1", "")
+	fset.Int("flag-2", 20, "")
+
+	ctx := ucli.NewContext(makeApp(), fset, nil)
+
+	err := builder.MakeAction(fakeAction{})(ctx)
+	require.NoError(t, err)
+
+	data := string(calls.Get(0, 0).([]byte))
+	require.Equal(t, "\x00\x00"+`{"flag-1":["item 1","item 2"],"flag-2":20}`, data)
+
+	builder.daemonFactory = fakeFactory{err: fake.GetError()}
+	err = builder.MakeAction(fakeAction{})(ctx)
+	require.EqualError(t, err, fake.Err("couldn't make client"))
+
+	builder.daemonFactory = fakeFactory{errClient: fake.GetError()}
+	err = builder.MakeAction(fakeAction{})(ctx)
+	require.EqualError(t, err, fake.GetError().Error())
+}
+
+func TestCliBuilder_Build(t *testing.T) {
+	builder := &CLIBuilder{
+		actions:       &actionMap{},
+		daemonFactory: fakeFactory{},
+		inits:         []Initializer{fakeInitializer{}},
+	}
+
+	cb := builder.SetCommand("test")
+	cb.SetDescription("test description")
+	cb.SetAction(builder.MakeAction(fakeAction{}))
+	cb.SetFlags(cli.StringFlag{Name: "string-flag"})
+
+	sub := cb.SetSubCommand("subtest")
+	sub.SetDescription("subtest description")
+	sub.SetFlags(cli.DurationFlag{}, cli.IntFlag{}, cli.StringSliceFlag{})
+
+	cb = builder.SetCommand("another")
+	cb.SetAction(func(cli.Flags) error {
+		return nil
+	})
+
+	cb = builder.SetCommand("last")
+	cb.SetAction(func(cli.Flags) error {
+		return fake.GetError()
+	})
+
+	app := builder.Build().(*ucli.App)
+	require.Len(t, app.Commands, 4)
+
+	// Check the referencing of the actions.
+	err := app.Commands[1].Action(nil)
+	require.NoError(t, err)
+}
+
+func TestCliBuilder_UnknownType_BuildFlags(t *testing.T) {
+	defer func() {
+		r := recover()
+		require.Equal(t, "flag type '<nil>' not supported", r)
+	}()
+
+	builder := &CLIBuilder{}
+	builder.SetStartFlags((cli.Flag)(nil))
+
+	builder.buildFlags(builder.startFlags)
+}
+
+// -----------------------------------------------------------------------------
+// Utility functions
+
+func makeApp() *ucli.App {
+	return &ucli.App{
+		Flags: []ucli.Flag{
+			&ucli.StringSliceFlag{Name: "flag-1"},
+			&ucli.IntFlag{Name: "flag-2"},
+		},
+	}
+}
